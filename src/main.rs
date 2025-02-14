fbecd32dabfc5bf25a20796a7208ac47c805ea72
luciano093 <luparedes100@gmail.com>
2025-02-14 10:38:42 -0500

    Add server discovery

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::io::Read;
+
+use serde_json::Value;
+
+#[derive(Debug, Default)]
+struct Server {
+    delegated_name: String,
+
+}
+
+impl Server {
+    fn connect(url: &str) {  
+        // get delegated server name
+        let mut res = reqwest::blocking::get(format!("{}/.well-known/matrix/server", url)).unwrap();
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+
+        let mut server = Server::default();
+        server.delegated_name = serde_json::from_str::<Value>(&body).unwrap()["m.server"].as_str().unwrap().to_owned();
+
+        println!("m.server: {}", server.delegated_name);
+
+        // get server implemenation name and version
+        let mut res = reqwest::blocking::get(format!("https://{}/_matrix/federation/v1/version", server.delegated_name)).unwrap();
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+
+        let implementation_name = serde_json::from_str::<Value>(&body).unwrap()["server"]["name"].to_string();
+        let implementation_version = serde_json::from_str::<Value>(&body).unwrap()["server"]["version"].to_string();
+
+        println!("implementation name: {} implementation version: {}", implementation_name, implementation_version);
+
+        // get server published signing keys
+        let mut res = reqwest::blocking::get(format!("{}/_matrix/key/v2/server", url)).unwrap();
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+        let res = serde_json::from_str::<Value>(&body).unwrap();
+
+        println!("server_name: {}\nsignatures: {}\nvalid_until_ts: {}\nverify_keys: {}", res["server_name"], res["signatures"], res["valid_until_ts"], res["verify_keys"]);
+
+
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Server::connect("https://matrix.org");    
 }
