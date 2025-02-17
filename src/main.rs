3948c3812df6ab700930cb5b028ac7658cf8b734
luciano093 <luparedes100@gmail.com>
2025-02-17 14:44:16 -0500

    Add key generation and loading logic for ed25519 keys

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,57 @@
-use std::io::Read;
+use std::fs::{create_dir, File};
+use std::io::{Read, Write};
+use std::path::Path;
 
+use base64::Engine;
+use ed25519_dalek::{SigningKey, VerifyingKey};
+use rand::rngs::OsRng;
 use serde_json::Value;
 
 #[derive(Debug, Default)]
 struct Server {
     delegated_name: String,
-
 }
 
 impl Server {
     fn connect(url: &str) {  
+        if !Path::new("./.keys").exists() {
+            create_dir("./.keys").unwrap();
+        }
+
+        let private_key = if Path::new("./.keys/id_ed25519").exists() {
+            let mut bytes = [0; 32];
+
+            File::open("./.keys/id_ed25519").unwrap().read_exact(&mut bytes).unwrap();
+
+            SigningKey::from_bytes(&bytes)
+        } else {
+            let mut csprng = OsRng;
+            let private_key = SigningKey::generate(&mut csprng);
+            let mut private_key_file = File::create_new("./.keys/id_ed25519").unwrap();
+
+            private_key_file.write_all(private_key.as_bytes()).unwrap();
+
+            private_key
+        };
+
+        let public_key = if Path::new("./.keys/id_ed25519.pub").exists() {
+            let mut bytes = [0; 32];
+
+            File::open("./.keys/id_ed25519.pub").unwrap().read_exact(&mut bytes).unwrap();
+
+            VerifyingKey::from_bytes(&bytes).unwrap()
+        } else {
+            let public_key = VerifyingKey::from(&private_key);
+            
+            let mut public_key_file = File::create_new("./.keys/id_ed25519.pub").unwrap();
+            public_key_file.write_all(public_key.as_bytes()).unwrap();
+
+            public_key
+        };
+        
+        println!("private_key: {}", base64::prelude::BASE64_STANDARD.encode(private_key.to_bytes()));
+        println!("public_key: {}", base64::prelude::BASE64_STANDARD.encode(public_key.to_bytes()));
+
         // get delegated server name
         let mut res = reqwest::blocking::get(format!("{}/.well-known/matrix/server", url)).unwrap();
         let mut body = String::new();
